e221b0a8e98c4e025645050e968aef97adf7e874
marcyhel <84603176+marcyhel@users.noreply.github.com>
2022-03-12 19:12:37 -0300

    Update flasks.py

--- flasks.py
+++ flasks.py
@@ -1,7 +1,15 @@
-from flask import Flask
+import os
+from flask import Flask, jsonify, request
+
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/')
+def nao_entre_em_panico():
+    if request.headers.get('aa') == '42':
+        return jsonify({"42": "a resposta para a vida, o universo e tudo mais"})
+    return jsonify({"message": "hello Word"})
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
